819a2fc604d9860b3bf351f745fc1c3ce07c5350
Magnus Allison <magnus_alli@hotmail.co.uk>
2025-05-24 22:16:16 +0100

    update to lastest node

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,12 @@
 import { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  document.getElementById('root')
+const root = createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(
+	<StrictMode>
+		<App />
+	</StrictMode>
 );
-
-
-reportWebVitals();
